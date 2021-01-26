274837047eaddb133d769b68e3ac9f903758f993
Michal Turecki <turecki@gmail.com>
2021-01-26 05:58:09 +0000

    Added Avalonia app project

---
+++ KeePassCore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeePassCore.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ KeePassCore/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace KeePassCore.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
